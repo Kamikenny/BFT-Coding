b2eddd36f5b763c16847845b9fe7f614b8074d58
Kenny Sirichantho <76822808+Kamikenny@users.noreply.github.com>
2026-05-05 15:23:40 +0200

    Add files via upload

--- conditions.js
+++ conditions.js
@@ -63,7 +63,9 @@ if (fruit.toLowerCase().includes("banane")) {
   console.log(`Je ne connais pas ce fruit : "${fruit}"`);
 }
  */
-// --------- EXTRAS --------
+// --------------------------------------------
+// --------- EXTRAS ---------------------------
+// --------------------------------------------
 
 //Exercice 1
 /* 
@@ -107,5 +109,144 @@ if (
   alert("Le magasin est ouvert.");
 }
  */
+// --------------------------------------------
+// ----- EXTRAS 12b ---------------------------
+// --------------------------------------------
 
-//Exercice 4
+//  1. Calculateur d'Indice de Masse Corporelle (IMC) :
+/* 
+const poids = parseFloat(prompt("Quel est votre poids (en Kg) ? :"));
+const taille = parseFloat(prompt("Quelle est votre taille (en mètres) ? :"));
+const imc = (poids / taille ** 2).toFixed(1);
+
+console.log(`Votre IMC est : ` + imc);
+
+if (isNaN(imc)) {
+  alert("Une erreur s'est produite");
+} else if (imc < 18.5) {
+  console.log("Selon votre IMC, vous êtes en insuffisance pondérale");
+} else if (imc < 25) {
+  console.log("Selon votre IMC, vous êtes en poids normal");
+} else {
+  console.log("Selon votre IMC, vous êtes en surpoids");
+}
+ */
+
+// 2. Validateur de Code Promotionnel Simple :
+/* 
+const prix = parseInt(prompt("Quel est le prix de l'article ? :"));
+const reduction = prompt("Avez-vous un code de réduction ? (oui / non) :").toLowerCase();
+
+if (reduction.includes("oui")) {
+  const code = prompt("Quel est votre code de réduction ? :").toUpperCase();
+
+  if (code === "REDUC15") {
+    console.log(
+      `Grâce à la réduction, votre article coûte ${prix - (prix * 15) / 100}`
+    );
+  } else if (code === "SUPER50") {
+    console.log(`Grâce à la réduction, votre article coûte ${prix / 2}`);
+  } else {
+    console.log(`Code invalide. Aucune réduction appliquée.`);
+  }
+} else if (reduction.includes("non")) {
+  console.log(
+    "Aucune réduction. Le prix de votre artcile est toujours " + prix
+  );
+}
+ */
+
+// 3. Extracteur de Milieu de Mot :
+/* 
+const mot = prompt("Veuillez entrer un mot :");
+
+if (mot.length % 2 === 0) {
+  console.log("Votre mot a un nombre de caractères pair.");
+  console.log(
+    "Voici les deux caractères du milieu : " +
+      mot.slice(mot.length / 2 - 1, mot.length / 2 + 1)
+  );
+} else {
+  console.log("Votre mot a un nombre de caractères impair.");
+  console.log(
+    "Voici le caractère du milieu : " + mot[Math.floor(mot.length / 2)]
+  );
+}
+ */
+
+// 4. Vérificateur de Symétrie de Mot (Début/Fin) :
+/* 
+const mot = prompt("Veuillez entrer un mot :");
+
+if (mot[0].toLowerCase() === mot[mot.length - 1].toLowerCase()) {
+  console.log("Le premier et le dernier caractères sont identiques.");
+} else {
+  console.log("Le premier et le dernier caractères sont différents.");
+}
+ */
+
+// 5. Générateur de Nombres dans une Plage Spécifique :
+/* 
+let number = Math.floor(Math.random() * (79 - 21 + 1) + 21); //Math.floor(Math.random() * (maxInclus - minInclus + 1) + minInclus)
+console.log(number);
+ */
+
+// 6. Analyseur de Force de Mot de Passe Basique :
+/* 
+const pass = prompt(
+  "Veuillez proposer un mot de passe, je vais analyser sa force :"
+);
+
+if (pass.length < 6) {
+  console.log("Votre mot de passe est faible.");
+} else if (6 <= pass.length && pass.length <= 9) {
+  console.log("Votre mot de passe est moyen.");
+} else {
+  console.log("Votre mot de passe est fort.");
+}
+ */
+
+// 7. Séparateur de Composants de Date :
+/* 
+const date = prompt("Veuillez entrer une date (format JJ/MM/AAAA) :");
+
+console.log("Jour : " + date.slice(0, 2));
+console.log("Jour : " + date.slice(3, 5));
+console.log("Jour : " + date.slice(6, date.length));
+ */
+
+// 8. Comparateur de Longueurs de Chaînes :
+/* 
+const un = prompt("Veuillez entrer un premier STRING:");
+const deux = prompt("Veuillez entrer un second STRING:");
+
+if (un.length > deux.length) {
+  console.log(
+    `Le premier string est plus long de ${un.length - deux.length} caractères.`
+  );
+} else if (un.length === deux.length) {
+  console.log("Les deux strings ont la même longueur");
+} else {
+  console.log(
+    `Le second string est plus long de ${deux.length - un.length} caractères.`
+  );
+}
+ */
+
+// 9. Calculateur d'Hypoténuse :
+/* 
+const un = prompt(
+  "Veuillez entrer la longueur du premier côté de l'angle droit (en cm) :"
+);
+const deux = prompt(
+  "Veuillez entrer la longueur du second côté de l'angle droit (en cm) :"
+);
+
+const hypotenuse = Math.sqrt(un * un + deux * deux);
+
+console.log(
+  `La longueur de l'hypoténuse de votre triangle est : ${hypotenuse} cm.`
+);
+ */
+
+// 10. Détermination de Quadrant Cartésien :
