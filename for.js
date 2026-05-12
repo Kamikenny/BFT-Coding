9caa9fce68b8439be1306b413062a1fc1feda48a
Kamikenny <sirichantho.kenny@gmail.com>
2026-05-12 15:27:47 +0200

    added for.js

---
+++ for.js
@@ -0,0 +1,38 @@
+// 2. Afficher les nombres pairs de 2 à 20
+/* 
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i} est un nombre pair.`);
+  }
+}
+ */
+
+// 3. Parcourir un tableau
+/* 
+const animaux = ["a", "b", "c", "d", "e"];
+
+for (let i = 0; i < animaux.length; i++) {
+  console.log(animaux[i]);
+}
+ */
+
+// 4. Calculer la somme des nombres de 1 à 100
+/* 
+let sum = 0;
+
+for (let i = 0; i <= 100; i++) {
+  console.log(`Total actuel : ${sum}`);
+  sum += i;
+}
+
+console.log(`Total final : ${sum}`);
+ */
+
+// 5. Inverser un tableau
+/* 
+const lettres = ["a", "b", "c", "d", "e"];
+
+for (let i = lettres.length - 1; i >= 0; i--) {
+  console.log(lettres[i]);
+}
+ */
